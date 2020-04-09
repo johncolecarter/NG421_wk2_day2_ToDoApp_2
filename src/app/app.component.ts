820c1e87b46973bca8112e765f626dfa9553cb1f
johncolecarter <colecarter7777@gmail.com>
2020-04-09 10:19:27 -0500

    finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,39 +10,11 @@ import { TodoService } from './services/todo.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  // title = 'Todos';
-  // todoList: ITodo[] = [];
-  // todoTitle: string;
-  // todoId = 0;
 
   constructor(private todoService: TodoService) {
 
   }
   ngOnInit() {
   }
-  // addTodo(): void {
-  //   this.todoList.push({
-  //     id: this.todoId,
-  //     title: this.todoTitle,
-  //     description: ''
-  //   });
-
-  //   this.todoTitle = '';
-  //   this.todoId++;
-
-  // }
-  // // resets our todoTitle variable to an empty strin
-  // async deleteTodo(todo: any) {
-  //   const modal = this.modalService.open(ConfirmationModalComponent);
-  //   const component: ConfirmationModalComponent = modal.componentInstance;
-  //   component.modalInstance = modal;
-
-  //   const result = await modal.result;
-
-  //   if (result === 'yes') {
-  //     const index = this.todoList.findIndex(todoItem => todoItem === todo);
-  //     this.todoList.splice(index, 1);
-  //   }
-
 }
 
